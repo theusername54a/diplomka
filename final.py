465dafdef9e70486a059644c112bd2d8898edc3a
theusername54a <163005703+theusername54a@users.noreply.github.com>
2024-03-14 09:52:17 +0100

    Update final.py

--- final.py
+++ final.py
@@ -1,16 +1,27 @@
-import pyspx.shake_128s
-import random
-import string
-
 import os
+import importlib
 
-seed = os.urandom(48)
+def main():
+    # Název modulu uložený v proměnné
+    prom = "pyspx.shake_128s"
+    
+    # Importovat modul na základě názvu v proměnné
+    algorithm_module = importlib.import_module(prom)
 
+    if algorithm_module:
+        # Generování náhodného klíče
+        seed = os.urandom(48)
+        # Podepisování zprávy
+        message = "hello"
+        message = message.encode()
+        public_key, secret_key = algorithm_module.generate_keypair(seed)
+        signature = algorithm_module.sign(message, secret_key)
+        
+        # Ověření zprávy
+        message = "hello8"
+        message = message.encode()
+        print(algorithm_module.verify(message, signature, public_key))
 
-message ="hello"
-message = message.encode()
-public_key, secret_key = pyspx.shake_128s.generate_keypair(seed)
-signature = pyspx.shake_128s.sign(message, secret_key)
-message ="hello"
-message = message.encode()
-print(pyspx.shake_128s.verify(message, signature, public_key))
+if __name__ == "__main__":
+    main()
+
